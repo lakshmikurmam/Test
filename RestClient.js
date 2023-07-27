32ffba3e738ac3a32156dde0bdabdec11ecc8363
Lakshmi <lakshmikurmam2020@outlook.com>
2023-07-27 19:32:07 +0530

    changes pushed

--- RestClient.js
+++ RestClient.js
@@ -1,23 +1,15 @@
-const { response } = require("express");
-
- 
+const fetch = require("node-fetch");
 
 const url = "http://10.246.0.10/MC/V1/OSPMS/DRIVE/E0010/Power_Consumed_KW";
 
- 
-
 async function getData(urlPath) {
-
-  let response = await fetch(urlPath);
-
-  let data = await response.json();
-
-  console.log(data);
-
+  try {
+    let response = await fetch(urlPath);
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
 }
 
- 
-
 getData(url);
-
- 
